e9c80d0ee05d4231925c075761bc4231dd8a70b5
devtiberio <tiberiope@gmail.com>
2022-04-06 19:27:29 -0300

    Delimitando movimento na tela

--- ship.py
+++ ship.py
@@ -1,9 +1,10 @@
 import pygame
 
 class Ship():
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         '''Inicializa a nave e define a sua posição inicial'''
         self.screen = screen
+        self.ai_settings = ai_settings
 
         # Carrega a imagem da nava e obtém o seu rect.
         self.image = pygame.image.load('images/ship.bmp')
@@ -14,6 +15,10 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
+        # Cria as coordenadas com valor decimal.
+        self.centerx = float(self.rect.centerx)
+        self.centery = float(self.rect.bottom)
+
         # Carrega as flags de movimento.
         self.moving_right = False
         self.moving_left = False
@@ -22,17 +27,21 @@ class Ship():
 
     def update(self):
         '''Atualiza a posição da nave de acordo com a flag de movimento.'''
-        if self.moving_right:
-            self.rect.centerx += 1
+        if (self.moving_right) and (self.rect.right < self.screen_rect.right):
+            self.centerx += self.ai_settings.ship_speed_factor
 
-        elif self.moving_left:
-            self.rect.centerx -= 1
+        elif (self.moving_left) and (self.rect.left > 0):
+            self.centerx -= self.ai_settings.ship_speed_factor
 
-        elif self.moving_up:
-            self.rect.centery -= 1
+        if (self.moving_up) and (self.rect.top > 5):
+            self.centery -= self.ai_settings.ship_speed_factor
 
-        elif self.moving_down:
-            self.rect.centery += 1
+        elif (self.moving_down) and (self.rect.bottom < self.screen_rect.bottom):
+            self.centery += self.ai_settings.ship_speed_factor
+
+        # Atualiza o objeto de acordo com o centerx e centery.
+        self.rect.centerx = self.centerx
+        self.rect.bottom = self.centery
 
     def blitme(self):
         '''Desenha a nave em sua posição atual.'''
